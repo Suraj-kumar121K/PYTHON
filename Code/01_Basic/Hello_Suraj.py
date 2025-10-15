d2ae45589036f4b490855049243c69f226bb242d
Suraj Kumar <surajkumar09464@gmail.com>
2025-10-16 00:36:25 +0530

    Refactor Hello_Suraj.py with function and import
    Refactor the Hello_Suraj.py script to include a function for printing names and import functionality from another file.

--- Code/01_Basic/Hello_Suraj.py
+++ Code/01_Basic/Hello_Suraj.py
@@ -1,7 +1,14 @@
-# First Code
+-->  First Program
 print("Suraj Kumar");
 
+--> Change The Print Name
 def suraj(n);
     print(n)
 
 suraj("Hello Suraj");
+
+--> Import File
+# From 1st File and Import Using File
+from suraj import Hello_suraj
+
+suraj("I am Suraj");
