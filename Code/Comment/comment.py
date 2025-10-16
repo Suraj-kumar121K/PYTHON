c96fd9e773d07266280927ff4d84bab7e8ab8516
Suraj Kumar <surajkumar09464@gmail.com>
2025-10-17 03:46:21 +0530

    Update and rename comment.py to – MODULES_COMMENTS_PIP.py

--- Code/Comment/comment.py
+++
@@ -1,6 +0,0 @@
---> Single Line Comment
-# This is a single line comment
---> Multiline Comment
-"""
-This is a Multiline comment
-"""
---
+++ Code/Comment/– MODULES_COMMENTS_PIP.py
@@ -0,0 +1,45 @@
+--> WHAT IS PROGRAMMING?
+''' Just like we use Hindi or English to communicate with each other, we use a
+programming language like Python to communicate with the computer.'''
+
+--> WHAT IS PYTHON?
+''' Python is a simple and easy to understand language which feels like reading simple
+English. This Pseudo code nature is easy to learn and understandable by beginners.'''
+
+--> FEATURES OF PYTHON
+'''
+• Easy to understand = Less development time
+• Free and open source
+• High level language
+• Portable: Works on Linux / Windows / Mac.
+• Fun to work with!
+'''
+
+--> MODULES
+''' A module is a file containing code written by somebody else (usually) which can be
+imported and used in our programs.'''
+--> PIP
+''' Pip is the package manager for python. You can use pip to install a module on your
+system.'''
+--> pip install flask # Installs Flask Module
+--> pip install pyjokes # Installs pyjokes Module
+--> pip install pyttsx3 # Installs pyttsx3 Module
+
+--> TYPES OF MODULES
+''' There are two types of modules in Python.'''
+--> 1. Built in Modules (Preinstalled in Python)
+--> 2. External Modules (Need to install using pip)
+'''Some examples of built in modules are os, random etc.
+Some examples of external modules are tensorflow, flask etc.
+USING PYTHON AS A CALCULATOR
+We can use python as a calculator by typing “python” + ↵ on the terminal.
+This opens REPL or Read Evaluate Print Loop'''
+
+--> Comment
+--> Single Line Comment
+# This is a single line comment
+
+--> Multiline Comment
+"""
+This is a Multiline comment
+"""
