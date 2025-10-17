4d5206b04bb428a2c6f90ee1d18ba686f83eaadf
Suraj Kumar <surajkumar09464@gmail.com>
2025-10-17 23:45:55 +0530

    Create Variable_and_DataType.py

---
+++ Code/VARIABLES_AND_DATATYPES/Variable_and_DataType.py
@@ -0,0 +1,7 @@
+--> What is Variable
+    A variable is the name given to a memory location in a program. For example
+a = 12          # variables = container to store a value.
+b = 1.73        # identifiers = class/function/variable name
+Name = "suraj"  # keywords = reserved words in python
+print(a + b);
+print(Name); 
