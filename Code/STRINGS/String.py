5ab46e0b5f25b680e7bec4c74131209ca74e305b
Suraj Kumar <surajkumar09464@gmail.com>
2025-10-18 00:12:30 +0530

    Create String.py

---
+++ Code/STRINGS/String.py
@@ -0,0 +1,35 @@
+--> What is String
+    a string is a sequence of characters enclosed in quotes.
+
+-->  You can use:
+     Single quotes ' '
+     Double quotes " "
+     Triple quotes ''' ''' or """ """ (for multi-line strings)
+
+--> 🟢 Example:
+        a ='suraj' # Single quoted string
+        b = "suraj" # Double quoted string
+        c = '''suraj''' # Triple quoted string
+        d = """suraj""" # Multiline quoted Line
+
+--> ✍️ 2. String Creation
+          You can create strings in different ways:
+a = 'Python'
+b = "Learning"
+c = '''Data Science'''
+
+--> 🔤 3. Accessing Characters
+          Strings are index-based — each character has a position (index).
+
+Character      --	 P	 y	 t  	h  	o  	n
+                   -----------------------
+Index	         --  0	 1	 2	  3	  4	  5
+                   ------------------------
+Negative Index --  (-5) (-4) (-3) (-2) (-1)
+
+--> The index in a sting starts from 0 to (length -1) in Python. In order to slice a string, we use
+
+--> Example:
+word = "Python"
+print(word[0])   # P
+print(word[5])   # n
