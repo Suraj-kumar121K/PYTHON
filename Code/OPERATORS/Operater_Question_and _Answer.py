4f8b691bcad0c54c24ae40514f6e56a1e876aeaf
Suraj Kumar <surajkumar09464@gmail.com>
2025-10-18 01:27:02 +0530

    Create Operater_Question_and _Answer.py

---
+++ Code/OPERATORS/Operater_Question_and _Answer.py
@@ -0,0 +1,115 @@
+--> 🧠 50 Important Python Operator Questions
+
+--> 🤔A. Arithmetic Operators (1–10)
+
+1. What are the arithmetic operators in Python?
+
+2. Write a program to add two numbers.
+
+3. What is the difference between / and // division?
+
+4. Find the remainder when 25 is divided by 4.
+
+5. What is the result of 2 ** 3?
+
+6. Evaluate: 10 + 5 * 2 - 3.
+
+7. Write a program to calculate the area of a circle.
+
+8. What is the output of:
+   x = 10
+   y = 3
+   print(x // y, x / y)
+
+9. Write a program to calculate average of three numbers.
+
+10. What is the output of:
+    print(5 + 3 * 2 ** 2)
+
+--> 🤔B. Assignment Operators (11–17)
+11. What does x += 5 do?
+
+12. Write a program to increment a variable by 10.
+
+13. What is the result of:
+    a = 10
+    a -= 3
+    print(a)
+
+14. Explain the use of *=, /=, //=, and **=.
+
+15. Write a program that multiplies user input by 5 using *=.
+
+16. Predict the output:
+    x = 4
+    x **= 2
+    print(x)
+
+17. What happens in this case?
+    a = 8
+    a //= 3
+    print(a)
+
+--> 🤔C. Comparison (Relational) Operators (18–25)
+
+18. List all relational operators in Python.
+
+19. Write a program to check if a number is positive.
+
+20. What is the result of 5 > 3 and 10 < 20?
+
+21. Evaluate:
+    x = 10
+    y = 20
+    print(x == y, x != y)
+
+22. What will be printed:
+    print(3 >= 3, 4 < 2)
+
+23. Write a program to compare two numbers entered by the user.
+
+24. What is the result of "apple" < "banana"?
+
+25. Explain how string comparison works in Python.
+
+--> 🤔D. Logical Operators (26–33)
+
+26. What are the logical operators in Python?
+
+27. What is the output of:
+    print(True and False)
+
+28. Write a program to check if a number is between 10 and 50 using and.
+
+29. What does or operator do? Give an example.
+
+30. What is the output of:
+    a = 5
+    b = 10
+    print(a > 2 or b < 5)
+
+31. Write a program to check if a number is not negative using not.
+
+32. What will be printed:
+    x = 0
+    print(not x)
+
+33. Combine two conditions using logical operators.
+
+--> 🤔E. Bitwise Operators (34–39)
+
+34. What are bitwise operators in Python?
+
+35. What is the binary representation of 5 and 3?
+
+36. Evaluate:
+a = 5
+b = 3
+print(a & b)
+
+37. What does | (OR) do in bitwise operation?
+
+38. Write a program to show ^ (XOR) operator.
+
+39. What will be the output of:
+    print(10 << 1, 10 >> 1)
