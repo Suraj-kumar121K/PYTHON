9e19fb66414f93d58b99f5b0ec62181f3eeebe59
Suraj Kumar <surajkumar09464@gmail.com>
2025-10-17 23:46:14 +0530

    Delete VARIABLES_AND_DATATYPES directory

--- VARIABLES_AND_DATATYPES/Variable_and_DataType.py
+++
@@ -1,7 +0,0 @@
---> What is Variable
-    A variable is the name given to a memory location in a program. For example
-""" a = 12      # variables = container to store a value.
-b = 1.73        # identifiers = class/function/variable name
-Name = "suraj"  # keywords = reserved words in python
-print(a + b);
-print(Name); """
