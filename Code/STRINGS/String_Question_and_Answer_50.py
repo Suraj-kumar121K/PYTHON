c482954b950f9624b3c99f762c2957442cc009e5
Suraj Kumar <surajkumar09464@gmail.com>
2025-10-30 23:20:19 +0530

    Refactor string questions for clarity and format

--- Code/STRINGS/String_Question_and_Answer_50.py
+++ Code/STRINGS/String_Question_and_Answer_50.py
@@ -1,16 +1,13 @@
-🧠 50 Important String Questions in Python
-________________________________________
-🔹 A. Basic Level (1–15)
-1.	What is a string in Python?
-        A string is a sequence of characters enclosed in quotes (' or ").
+1.  What is a string in Python?
+    A string is a sequence of characters enclosed in quotes (' or ").
 
-2.	How do you create a string in Python?
-         S = 'Suraj'
+2. How do you create a string in Python?
+   S = 'Suraj'
 
-3.	What is the difference between single, double, and triple quotes?
-        | Type of Quotes    | Example                        | Used For                             | Notes                                                         |
-        | ----------------- | ------------------------------ | ------------------------------------ | ------------------------------------------------------------- |
-        | **Single Quotes** | `'Hello'`                      | For short single-line strings        | You can include double quotes inside easily: `'He said "Hi"'` |
-        | **Double Quotes** | `"Hello"`                      | Also for single-line strings         | You can include single quotes easily: `"It's a nice day"`     |
-        | **Triple Quotes** | `'''Hello'''` or `"""Hello"""` | For multi-line strings or docstrings | Allows both single and double quotes inside without escaping  |
+3. What is the difference between single, double, and triple quotes?
+   | Type of Quotes    | Example                        | Used For                             | Notes                                                         |
+   | ----------------- | ------------------------------ | ------------------------------------ | ------------------------------------------------------------- |
+   | **Single Quotes** | `'Hello'`                      | For short single-line strings        | You can include double quotes inside easily: `'He said "Hi"'` |
+   | **Double Quotes** | `"Hello"`                      | Also for single-line strings         | You can include single quotes easily: `"It's a nice day"`     |
+   | **Triple Quotes** | `'''Hello'''` or `"""Hello"""` | For multi-line strings or docstrings | Allows both single and double quotes inside without escaping  |
 
