ef3ef418b0d7d2489d5daccd865e078f0e25e514
Suraj Kumar <surajkumar09464@gmail.com>
2025-11-02 11:49:24 +0530

    Create Functions.py

---
+++ Code/Functions_&_Recursions/Functions.py
@@ -0,0 +1,19 @@
+-->🔹What is a Function?
+A function is a block of code that runs only when it is called.
+
+--> ✅ Syntax:
+def function_name(parameters):
+    # body of the function
+    return value
+
+--> Example:
+def greet(name):
+    return "Hello, " + name
+print(greet("Suraj"))
+
+--> Types of Functions
+|          Type              |           Example            |           Description               |
+| -------------------------- | ---------------------------- | ----------------------------------- |
+| **Built-in Functions**     | `print()`, `len()`, `type()` | Already defined in Python           |
+| **User-defined Functions** | `def my_function():`         | Created by users                    |
+| **Lambda Functions**       | `lambda x: x*x`              | Small, one-line anonymous functions |
