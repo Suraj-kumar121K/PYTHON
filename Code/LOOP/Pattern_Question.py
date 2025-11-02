c469fbce458614fb7e04d12c0366e56ffbaf2939
Suraj Kumar <surajkumar09464@gmail.com>
2025-11-03 02:11:56 +0530

    Create Pattern_Question.py

---
+++ Code/LOOP/Pattern_Question.py
@@ -0,0 +1,151 @@
+🧩 1. Square Star Pattern
+*****
+*****
+*****
+*****
+*****
+
+🧩 2. Right Triangle Pattern
+*
+**
+***
+****
+*****
+
+🧩 3. Inverted Right Triangle
+*****
+****
+***
+**
+*
+
+🧩 4. Pyramid Pattern
+    *
+   ***
+  *****
+ *******
+*********
+
+🧩 5. Inverted Pyramid
+*********
+ *******
+  *****
+   ***
+    *  
+
+🧩 6. Diamond Pattern
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+
+🧩 7. Number Triangle
+1
+12
+123
+1234
+12345
+  
+🧩 8. Inverted Number Triangle
+12345
+1234
+123
+12
+1
+
+🧩 9. Continuous Number Triangle
+1
+2 3
+4 5 6
+7 8 9 10
+
+🧩 10. Character Triangle
+A
+AB
+ABC
+ABCD
+
+🧩 11. Reverse Character Triangle
+ABCD
+ABC
+AB
+A
+
+🧩 12. Hollow Square
+*****
+*   *
+*   *
+*   *
+*****
+
+🧩 13. Hollow Pyramid
+    *
+   * *
+  *   *
+ *     *
+*********
+
+🧩 14. Hourglass Pattern
+*********
+ *******
+  *****
+   ***
+    *
+   ***
+  *****
+ *******
+*********
+
+🧩 15. Right Pascal’s Triangle
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+🧩 16. Left Triangle Pattern
+    *
+   **
+  ***
+ ****
+*****
+
+🧩 17. Binary Number Pattern
+1
+01
+101
+0101
+
+🧩 18. Floyd’s Triangle
+1
+2 3
+4 5 6
+7 8 9 10
+  
+🧩 19. Pyramid of Numbers  
+    1
+   121
+  12321
+ 1234321
+
+🧩 20. Butterfly Pattern
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+
+
