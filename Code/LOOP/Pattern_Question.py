ac88f970bc2f0c745385636e81f08601aed96c52
Suraj Kumar <surajkumar09464@gmail.com>
2025-11-04 02:36:18 +0530

    Update Pattern_Question.py

--- Code/LOOP/Pattern_Question.py
+++ Code/LOOP/Pattern_Question.py
@@ -29,7 +29,15 @@ for i in range(1, rows + 1):
 * * *
 * * * *
 * * * * *
+* * * * * *
 
+--> CODE
+r = 6
+for i in range(1, r + 1):
+  for j in range(1, i + 1):
+    print("*", end=" ")
+  print() 
+  
 🧩 3. Inverted Right Triangle
 
 * * * * * * 
