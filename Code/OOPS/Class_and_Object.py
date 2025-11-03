2b85342ae3eb1408d673a8941a467d02ed8a4b0c
Suraj Kumar <surajkumar09464@gmail.com>
2025-11-03 23:36:55 +0530

    Update Class_and_Object.py

--- Code/OOPS/Class_and_Object.py
+++ Code/OOPS/Class_and_Object.py
@@ -1,1 +1,16 @@
 --> What is Class
+    Class is a blueprint for creating objects.
+
+--> Syntax
+    class ClassName:
+    # attributes
+    # methods
+      
+ --> Example
+class Employee:  # Class
+    name = "Suraj"
+    language = "Python"
+    salary = 100000
+
+suraj = Employee()   # Object
+print(suraj.name, suraj.language)  
