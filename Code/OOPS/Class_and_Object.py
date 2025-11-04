66c190b3f1c3e3913dd376190517e768f92b5a81
Suraj Kumar <surajkumar09464@gmail.com>
2025-11-04 22:17:44 +0530

    Update Class_and_Object.py

--- Code/OOPS/Class_and_Object.py
+++ Code/OOPS/Class_and_Object.py
@@ -1,3 +1,7 @@
+--> What is OOPS
+OOPS is a programming style that organizes code into classes and objects —
+making it easier to understand, reuse, and maintain.
+
 --> What is Class
     Class is a blueprint for creating objects.
 
