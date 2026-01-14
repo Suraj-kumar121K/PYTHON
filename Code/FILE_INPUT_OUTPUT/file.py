6646801939e74b9674e50f7f45efea9da4fa1e6e
Suraj Kumar <surajkumar09464@gmail.com>
2026-01-15 00:30:00 +0530

    add

--- Code/FILE_INPUT_OUTPUT/file.py
+++ Code/FILE_INPUT_OUTPUT/file.py
@@ -39,4 +39,41 @@ with open("file.txt", "w") as f:
         
 """Deleting File  
 import os
-os.remove("f.txt");  """    
+os.remove("f.txt");  """  
+
+"""with open("practice.txt", "w") as f:
+    f.write("Hi everyone\nwe are learning File I/O\n")
+    f.write("using Java.\nI like programming in java,")"""
+    
+"""with open("practice.txt", "r") as f:
+    data = f.read()
+        
+new_data = data.replace("Java", "Python") 
+print(new_data)       
+
+with open("practice.txt", "w") as f:
+    f.write(new_data)"""
+    
+# Search if the word "learning" exists in the file or not.
+def check_for_word():
+    word = "learning"
+    with open("practice.txt", "r") as f:
+        data = f.read()
+        if(word in data):
+            print("Found")
+        else:
+            print("not found") 
+# check_for_word()    
+
+# WAF to find in which line of the file does the word "learning" occur first.
+# Print -1 if word not found.           
+def check_for_line():
+    word = "learning"
+    data = True
+    line_no = 1
+    with open("practice.txt", "r") as f:
+        while data:
+            data = f.readline()
+            if(word in data):
+                print(line_no)
+            line_no += 1  
