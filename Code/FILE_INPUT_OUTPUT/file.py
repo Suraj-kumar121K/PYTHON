a0d7e9b1219d6e7ac1f8084bbc984cc32d412cf4
Suraj Kumar <surajkumar09464@gmail.com>
2026-01-14 23:58:27 +0530

    add

--- Code/FILE_INPUT_OUTPUT/file.py
+++ Code/FILE_INPUT_OUTPUT/file.py
@@ -1,5 +1,42 @@
-f = open("demo.txt", "r")
-data = f.read()
-print(data)
-print(type(data))
+""" Read File
+f = open("file.txt", "r")
+# data = f.read()
+# print(data)
+
+line1 = f.readline()
+print(line1)
+
+line2 = f.readline()
+print(line2)
+
+# print(type(data))
+
 f.close()
+        """
+        
+        
+"""WRITE FILE    
+  
+f = open("file.txt", "a") 
+
+f.write("Then I'll move to ReactJS")
+
+f.write("\n After that nodejs")
+
+f.close()
+        """
+        
+        
+"""With Open      
+with open("file.txt") as f:
+    data = f.read()
+    print(data) 
+    
+with open("file.txt", "w") as f:
+        f.write("new data")
+        """ 
+        
+        
+"""Deleting File  
+import os
+os.remove("f.txt");  """    
