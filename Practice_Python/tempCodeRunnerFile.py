3b71a999f95da1b623bba25af6a7c908bdc1802a
Suraj Kumar <surajkumar09464@gmail.com>
2026-04-04 01:16:37 +0530

    add

--- Practice_Python/tempCodeRunnerFile.py
+++ Practice_Python/tempCodeRunnerFile.py
@@ -1,5 +1,2 @@
-user = int(input("Enter a number "))
-if user >= 10 and user <= 50:
-    print("Number is in range")
-else:
-    print("Number is out of range")
+text = ['a','b','c','d']
+print(text[3])
