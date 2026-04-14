2feefaeda8e3d08a70b674fb1e5a16a0748f06a4
Suraj Kumar <surajkumar09464@gmail.com>
2026-04-14 23:41:50 +0530

    add

--- Code/Project_1/add_json.py
+++ Code/Project_1/add_json.py
@@ -1,3 +1,10 @@
+import os
+
+# oneDNN warning band karne ke liye
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+
+# extra TensorFlow warnings hide karne ke liye
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 import json
 import numpy as np
 import tensorflow as tf
@@ -8,8 +15,6 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.preprocessing import LabelEncoder
 
-with open("intents.json") as file:
+with open("Quick.json") as file:
     data = json.load(file)
-    
 print(data)
-
