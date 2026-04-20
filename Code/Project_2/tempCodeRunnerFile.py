63b8e161a43b543895182f24627a7801a754cd24
Suraj Kumar <surajkumar09464@gmail.com>
2026-04-21 01:52:02 +0530

    add

---
+++ Code/Project_2/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+menu.add_cascade(label="File", menu=file_menu)
+
+# file_menu.add_command("New", command=new_file)
+# file_menu.add_command("Open", command=open_file)
+# file_menu.add_command("Save", command=save_file)
+# file_menu.add_separator()
+# file_menu.add_command("Exit", command=root.quit)
