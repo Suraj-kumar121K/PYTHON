19c38783d52518e52249c9d8e3364684dac5acd3
Suraj Kumar <surajkumar09464@gmail.com>
2026-04-26 04:00:04 +0530

    add

---
+++ Practice_Python/bd.py
@@ -0,0 +1,69 @@
+# Factorial
+"""n = int(input("Enter n: "))
+T = 1
+for i in range(1, n + 1):
+    T *= i
+print(T)"""
+
+#  Print numbers divisible by 3
+"""n = int(input("Enter a: "))
+for i in range(1, n + 1):
+    if i % 3 == 0:
+        print(i)"""
+
+# Count even numbers
+"""n = int(input("Enter n: "))
+count = 0
+for i in range(1,n+1):
+    if i % 2 == 0:
+        count += 1
+print(count)"""
+
+#  Count odd numbers
+"""n = int(input("Enter a number "))
+count = 0
+for i in range(1, n + 1):
+    if i % 2 != 0:
+        count += 1
+print(count)"""
+
+#  Sum of even numbers
+"""n = int(input("Enter a number "))
+Total = 0
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        Total += i
+print(Total)"""
+
+# Sum of odd numbers
+"""n = int(input("Enter a number "))
+Total = 0
+for i in range(1, n + 1):
+    if i % 2 != 0:
+        Total += i
+print(Total)"""
+
+# Print squares
+"""n = int(input("Enter n: "))
+for i in range(1, n + 1):
+    print(i * i)"""
+    
+# Print cubes
+# n = int(input("Enter n: "))
+# for i in range(1, n + 1):
+#     print(i ** 3)
+    
+# Reverse table
+"""for i in range(10, 0, -1):
+    print("5 x", i, "=", 5*i)"""
+
+# Find average  
+"""n = int(input("Enter N: "))
+total = 0
+for i in range(1, n+1):
+    total += i
+    avg = total / n
+print(avg)"""
+
+# Multiples of 5
+
