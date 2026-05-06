7c00ffbb894f5dfc0533e18ac973aa881aa2308a
Suraj Kumar <surajkumar09464@gmail.com>
2026-05-06 09:49:43 +0530

    add

---
+++ Code/Functions_&_Recursions/Lambda_Function/01_lam.py
@@ -0,0 +1,12 @@
+# Add two numbers
+"""add = lambda a, b: a + b
+print(add(3,4))"""
+
+#  Use with map()
+"""nums = [1, 2, 3, 4]
+result = list(map(lambda x: x*2, nums))
+print(result)"""
+
+words = ["Python", "is", "easy"]
+result = " ".join(words)
+print(result)
