78493404cf37bbfe92eadc8ff2b60445b9feaf68
Suraj Kumar <surajkumar09464@gmail.com>
2026-05-18 01:53:49 +0530

    add

--- Code/Todo_project/Todo_list_01.py
+++ Code/Todo_project/Todo_list_01.py
@@ -1,9 +1,9 @@
 todos = []
 add_new = True
-while add_new():
+while add_new:
     new_todo = input("Todo: ")
     todos.append(new_todo)
-    count = input("Continue? (Y/N)")
+    count = input("Continue? (Y/N): ")
     
     if count.lower() == "n":
         add_new = False
