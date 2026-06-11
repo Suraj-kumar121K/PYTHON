5a3baf47890166435680e65a511c9a66b1487213
Suraj Kumar <surajkumar09464@gmail.com>
2026-06-11 22:11:43 +0530

    add

--- Code/project_08/Weather_API.py
+++ Code/project_08/Weather_API.py
@@ -14,8 +14,43 @@ root.geometry("890x470+300+200")
 root.configure(bg="#57adff")
 root.resizable(False, False)
 
-#Icon
-Image_icon=photoImage(file="Image/loop.png")
-root.iconphoto(False,Image_icon)
+# Icon
+Image_icon = PhotoImage(file="../../icon/loop.png")
+root.iconphoto(False, Image_icon)
+
+# Rounded Box Image
+Round_box = PhotoImage(file="../../icon/Rounded_Rectangle_1.png")
+Label(root, image=Round_box, bg="#57adff").place(x=30, y=110)
+
+# Labels
+label1 = Label(root, text="Temperature",
+               font=('Helvetica',11),
+               fg="white",
+               bg="#203243")
+label1.place(x=50, y=120)
+
+label2 = Label(root, text="Humidity",
+               font=('Helvetica',11),
+               fg="white",
+               bg="#203243")
+label2.place(x=50, y=140)
+
+label3 = Label(root, text="Pressure",
+               font=('Helvetica',11),
+               fg="white",
+               bg="#203243")
+label3.place(x=50, y=160)
+
+label4 = Label(root, text="Wind Speed",
+               font=('Helvetica',11),
+               fg="white",
+               bg="#203243")
+label4.place(x=50, y=180)
+
+label5 = Label(root, text="Description",
+               font=('Helvetica',11),
+               fg="white",
+               bg="#203243")
+label5.place(x=50, y=200)
 
 root.mainloop()
