dde8f6609e5171d3e286c04bc2947377cd49c00f
Suraj Kumar <surajkumar09464@gmail.com>
2026-06-13 03:35:55 +0530

    add

--- Code/LOOP/pattern.py
+++ Code/LOOP/pattern.py
@@ -100,3 +100,127 @@ for i in range(n - 1, 0, -1):
         print("*", end="   ")
     print()
 """
+"""
+* * * * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * * * *
+"""
+"""
+* * * * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * * * *
+"""
+"""
+n = int(input("Enter a number: "))
+for i in range(n, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+for i in range(2, n + 1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+"""
+"""
+* * * * * *
+  * * * * *
+    * * * *
+      * * *
+        * *
+          *
+"""
+"""
+n = int(input("Entere a number "))
+for i in range(n, 0, -1):
+    for j in range(n - i):
+        print(" ", end=" ")
+    for k in range(i):
+        print("*", end=" ") 
+    print()
+"""
+"""
+* * * * * *
+  * * * * *
+    * * * *
+      * * *
+        * *
+          *
+        * *
+      * * * 
+    * * * *
+  * * * * *
+* * * * * *
+"""
+"""
+n = int(input("Entere a number ")) # i har baar decrease ho raha hai.
+for i in range(n, 0, -1):
+    for j in range(n - i):
+        print(" ", end=" ")
+    for k in range(i):
+        print("*", end=" ") 
+    print()
+for i in range(2, n + 1):
+    for j in range(n - i):
+        print(" ", end=" ")
+    for k in range(i):
+        print("*", end=" ") 
+    print()
+"""
+"""
+* * * * * * 
+ * * * * * 
+  * * * * 
+   * * * 
+    * * 
+     * 
+"""
+"""
+n = int(input("Entere a number "))
+for i in range(1, n + 1):
+    for j in range(i):
+        print(" ", end="")
+    for k in range(n - i):
+        print("*", end=" ")
+    print()
+"""
+
+"""
+     * 
+    * * 
+   * * * 
+  * * * * 
+ * * * * * 
+* * * * * *
+"""
+
+
+"""
+* * * * * * 
+ * * * * * 
+  * * * * 
+   * * * 
+    * * 
+     * 
+    * * 
+   * * * 
+  * * * * 
+ * * * * * 
+* * * * * *
+"""
