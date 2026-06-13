09e499bf306b874e3228670b96f9357e039939c1
Suraj Kumar <surajkumar09464@gmail.com>
2026-06-14 01:57:02 +0530

    add

--- Code/LOOP/pattern.py
+++ Code/LOOP/pattern.py
@@ -209,7 +209,13 @@ for i in range(1, n + 1):
  * * * * * 
 * * * * * *
 """
-
+"""n = int(input("Entere a number "))
+for i in range(1, n + 1):
+    for j in range(n - i):
+        print(" ", end="")
+    for k in range(i):
+        print("*", end=" ")
+    print()"""
 
 """
 * * * * * * 
@@ -224,3 +230,54 @@ for i in range(1, n + 1):
  * * * * * 
 * * * * * *
 """
+"""
+n = int(input("Entere a number "))
+for i in range(1, n + 1):
+    for j in range(i - 1):
+        print(" ", end="")
+    for k in range(n - i + 1):
+        print("*", end=" ")
+    print()
+for i in range(2, n + 1):
+    for j in range(n - i):
+        print(" ", end="")
+    for k in range(i):
+        print("*", end=" ")
+    print()
+"""
+    
+"""
+* * * * * * * * * * * * 
+* * * * *     * * * * *
+* * * *         * * * *
+* * *             * * *
+* *                 * *
+*                     * 
+* *                 * *
+* * *             * * *
+* * * *         * * * *
+* * * * *     * * * * *
+* * * * * * * * * * * *
+"""
+
+"""
+n = int(input("Enter a Number: "))
+for i in range(1, n + 1):
+    for j in range(1, 2*n):
+        if i == n or i + j == n + 1 or j - i == n - 1:
+            print("* ", end=" ")
+        else:
+            print(" ", end="  ")
+    print()
+"""   
+"""
+n = int(input("Enter a Number: "))
+for i in range(1, n + 1):
+    for j in range(1, 2 * n):
+        if i == n or i + j == n + 1 or j - i == n - 1:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+"""
+
