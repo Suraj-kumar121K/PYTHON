36630d6561d3023211fab3a4e6a6a2ac14292a98
Suraj Kumar <surajkumar09464@gmail.com>
2026-07-02 01:55:27 +0530

    add

--- Code/project_10/Calculater.py
+++ Code/project_10/Calculater.py
@@ -51,3 +51,70 @@ while True:
         break"""
         
 # 3. Table Generator
+
+"""def table(num):
+    for i in range(1, 11):
+        print(f"{i} x {num} =", num * i)
+while True:
+    num = int(input("Enter a Number: "))
+    print(table(num))
+    
+    choice = input("Enter Yes/No: ")
+    if choice == "Yes":
+        print("End Table")
+        break
+"""
+
+# Student Result System
+"""def student(total):
+    if total >= 90:
+        return "Gread A"
+    elif total >= 60:
+        return "Gread B"
+    elif total >= 35:
+        return "Gread C"
+    else:
+        return "Fail"
+    
+while True:
+    num = int(input("Enter a Number: "))
+    print(student(num))
+    
+    stop = input("Stop Yes/No: ")
+    if stop == "Yes":
+        print("stop Program")
+        break
+"""    
+
+# 5. Simple ATM Machine
+"""balance = int(input("Enter a Amount: "))
+def Check_Balance():
+    print("Blance ",balance)
+
+def withdraw(Amount):
+    global balance
+    if Amount <= balance:
+        balance -= Amount
+        print("Withdraw Amount")
+    else:
+        print("Insufficient Balance")
+
+while True:
+    print("\n1.Check Balance")
+    print("2.Withdraw")
+    print("3.Exit")
+    
+    choice = input("Enter a Number: ")
+    if choice == "1":
+        Check_Balance()
+        
+    elif choice == "2":
+        Amount = int(input("Enter a Amount:- "))
+        withdraw(Amount)
+        
+    elif choice == "3":
+        print("End Program")
+        break
+    else:
+        print("Invalid Choice")
+"""
