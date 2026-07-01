3a29b58f9fb48431bedc34ec5a263d133c70dde3
Suraj Kumar <surajkumar09464@gmail.com>
2026-07-02 00:37:01 +0530

    add

---
+++ Code/project_10/Calculater.py
@@ -0,0 +1,51 @@
+# Calculator Project
+"""print("welcome to calculater")
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+while True:
+    print("\n1.Add")
+    print("2.Subtract")
+    print("3.Multiply")
+    print("4.Divide")
+    print("5.Exit")
+    choice = input("Enter a Number: ")
+    if choice == "5":
+        print("Program End")
+        break
+    a = int(input("Enter a Number:- "))
+    b = int(input("Enter a Number:- "))
+    if choice == "1":
+        print("Add Number: ", add(a, b))
+    elif choice == "2":
+        print("Subtraction: ",sub(a, b))
+    if choice == "3":
+        print("multiply: ", mul(a, b))
+    if choice == "4":
+        print("Divide: ", div(a, b))
+    else:
+        print("Invalid Choice")"""
+        
+# check even and Odd
+"""def check(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+while True:
+    num = int(input("Enter a Number: "))
+    print(check(num))
+    stop = input("Stop Yes/No: ")
+    if stop == "Yes":
+        print("Program Stopped")
+        break"""
