8d73b9431cb6c9dc188a5a650d539a97ecf68617
Suraj Kumar <surajkumar09464@gmail.com>
2026-08-22 01:15:13 +0530

    add

---
+++ vvi/1_question.py
@@ -0,0 +1,50 @@
+# Number & Logic
+# 1. Reverse a number 
+num = 12345163
+rev = 0
+while num > 0:
+    digit = num % 10
+    rev = rev * 10 + digit 
+    num = num // 10
+print(rev)
+
+# 2. Check Palindrome Number 
+# 3. Check Prime Number 
+# 4. Print Prime Numbers from 1 to N 
+# 5. Find Factorial of a Number 
+# 6. Find Fibonacci Series 
+# 7. Check Armstrong Number 
+# 8. Find GCD/HCF of Two Numbers 
+# 9. Find LCM of Two Numbers 
+# 10. Find Sum of Digits
+
+# String — Very Important
+# 11.	Reverse a String 
+# 12.	Check Palindrome String 
+# 13.	Count Vowels and Consonants 
+# 14.	Count Character Frequency 
+# 15.	Find First Non-Repeating Character 
+# 16.	Check Two Strings are Anagrams 
+# 17.	Remove Duplicate Characters 
+# 18.	Find Duplicate Characters 
+# 19.	Find Largest Word in a String 
+# 20.	Count Words in a Sentence
+# Very Important
+# 21.	Find Largest Element Without max() 
+# 22.	Find Second Largest Element 
+# 23.	Remove Duplicates from List 
+# 24.	Find Duplicate Elements 
+# 25.	Find Missing Number from List
+
+# Very Important Interview Logic
+# 26.	26. Two Sum Problem 
+# 27.	Three Sum Problem 
+# 28.	Find Maximum Subarray Sum 
+# 29.	Find Common Elements in 3 Lists 
+# 30.	Merge Two Sorted Lists 
+# 31.	Remove Duplicates from Sorted List 
+# 32.	Find Majority Element 
+# 33.	Find All Pairs with Given Sum 
+# 34.	Rotate a List by K Positions 
+# 35.	Find Missing and Duplicate Number
+
