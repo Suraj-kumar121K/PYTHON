7a72f5d0e2eb731be6bd8d8831446ffee5ccd1b5
Suraj Kumar <surajkumar09464@gmail.com>
2026-08-23 22:39:08 +0530

    add

--- vvi/1_question.py
+++ vvi/1_question.py
@@ -1,15 +1,29 @@
 # Number & Logic
 # 1. Reverse a number 
-"""
-num = 12345163
-rev = 0
-while num > 0:
-    digit = num % 10
-    rev = rev * 10 + digit 
-    num = num // 10
-print(rev)
-"""
+def reverse(num):
+    rev = 0
+
+    while num > 0:
+        digit = num % 10
+        rev = rev * 10 + digit
+        num = num // 10
+    print(rev)
+# reverse(123456)
+
+
 # 2. Check Palindrome Number 
+def Palindrome(num):
+    original = num
+    reverse = 0
+    while num > 0:
+        digit = num % 10
+        reverse = reverse * 10 + digit
+        num = num // 10
+    if  original == reverse:
+        print("Palindrome Number ")
+    else:
+        print("Not Palindrome Number")
+# Palindrome(121)
 
 # 3. Check Prime Number 
 
@@ -32,11 +46,25 @@ def armstrong(num):
         print("Armstrong Number")
     else:
         print("Not Armstrong Number")
-armstrong(123)
+# armstrong(123)
 
 # 8. Find GCD/HCF of Two Numbers 
+def gcd(a, b):
+    smaller = min(a, b)
+    while smaller > 0:
+        if a % smaller == 0 and b % smaller == 0:
+            return smaller
+        smaller -= 1
+print(gcd(18, 12))
 
 # 9. Find LCM of Two Numbers 
+def lcm(a, b):
+    multiple = max(a, b)
+    while True:
+        if multiple % a == 0 and multiple % b == 0:
+            return multiple
+        multiple += 1
+# print(lcm(4, 6))
 
 # 10. Find Sum of Digits
 
